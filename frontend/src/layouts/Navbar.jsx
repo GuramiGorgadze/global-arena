89c017a2e47d965b6ece38728a2fabb26fa09f3f
GuramiGorgadze <g.gorgadze111@gmail.com>
2026-07-01 23:15:44 +0400

    added a single letter

--- frontend/src/layouts/Navbar.jsx
+++ frontend/src/layouts/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
           src={Logo}
           alt="Logo"
         />
-        <span>G-ARENA Natinal Summit 2026</span>
+        <span>G-ARENA National Summit 2026</span>
       </Link>
     </nav>
   );
