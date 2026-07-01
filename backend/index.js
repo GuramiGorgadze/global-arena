0cf1afc3ac51e1ac204973f4d4579f5ab2766b81
GuramiGorgadze <g.gorgadze111@gmail.com>
2026-07-01 20:41:06 +0400

    getting ready for hosting pt2

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import UsersRouter from "./routes/users.js";
 import connectDB from "./db/connection.js";
+import { fileURLToPath } from "url";
+import path from "path";
 
 dotenv.config();
 
@@ -18,6 +20,15 @@ app.use(
 app.use(express.json());
 
 app.use("/api/users", UsersRouter);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("/{*any}", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
 const startServer = async () => {
   await connectDB(process.env.CONNECTION_STRING);
