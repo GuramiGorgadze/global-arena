c0be24d1cf1dd702ce5c1e5ab6614bf180f1cdea
GuramiGorgadze <g.gorgadze111@gmail.com>
2026-08-30 15:35:14 +0400

    fixed promo

--- frontend/src/api/marathon.js
+++ frontend/src/api/marathon.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "/api/marathon";
+const BASE_URL = "http://localhost:3000/api/marathon";
 
 export const getMarathonStatus = async (email) => {
   try {
