0d23ca8dc1d9f0a03df12bc4c27e483c68a051ed
GuramiGorgadze <g.gorgadze111@gmail.com>
2026-09-10 20:15:51 +0400

    small fix

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,5 +2,4 @@ export { default as Home } from './Home';
 export { default as Registration } from './Registration';
 export { default as Marathon } from './Marathon';
 export { default as DelegateType } from './DelegateType';
-export { default as CommandPage } from './Command';
 export { default as Admin } from './Admin';
